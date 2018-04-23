f96867748d369eac7f9b9e1bee103e0b71e8266e
Peter <peterxp@hotmail.fr>
2018-04-23 12:16:08 +0200

    Imp

--- src/main/java/data/Location.java
+++ src/main/java/data/Location.java
@@ -2,6 +2,63 @@ package data;
 
 public class Location extends Searchable {
 
-    public String name;
+    private String name;
 
+    private String description;
+
+    private int floor;
+
+    private String landmark;
+
+    private double latitude;
+
+    private double longitude;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getLandmark() {
+        return landmark;
+    }
+
+    public void setLandmark(String landmark) {
+        this.landmark = landmark;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
